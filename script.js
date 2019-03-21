5558c1e9f45adeffce45d6bec8a36357dd79dced
suzuki86 <tsnr0001@gmail.com>
2019-03-22 01:11:19 +0900

    Add script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+addEventListener('load', function(){
+  document.getElementsByClassName('main-column')[0].style.maxWidth = '100%';
+  document.getElementsByClassName('main-column')[0].style.width = '100%';
+  document.getElementsByClassName('right-column')[0].style.display = 'none';
+});
